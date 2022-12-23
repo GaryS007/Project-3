03a055790b58608051b2c5ad92535ead5f849dab
GaryS007 <chattomeh@hotmail.com>
2022-12-23 17:43:07 +0000

    Add while loop to check if players guess is correct or incorrect. Also added replace_blank function

--- run.py
+++ run.py
@@ -1,9 +1,48 @@
 import random
 
-words = ['Orange', 'Apple']  # List of words
+words = ["orange", "apple"]  # List of words
 
-picked_word = random.choice(words)
+picked_word = random.choice(words)  # Picks a word from words
+blank_word = "_"*len(picked_word)
+length = len(picked_word)
 
-player_guess = input(f'Welcome to Hangman, your word is {picked_word}\n' + 'Guess a letter: ')
+print(
+    f"Your random word has {length} letters"
+    )
 
-print(player_guess)
+correct = ['_']*len(picked_word)
+incorrect = []
+
+
+def replace_blank():
+    """
+    This function is designed to replace the '_' 
+    in the "correct" variable with the player_guess in picked_word.
+    """
+    for i in correct:
+        print(i, end='')
+    print()
+
+
+while True:
+
+    print('....................................')
+
+    player_guess = input(
+        "Guess a letter: "
+    )
+
+    if player_guess in picked_word:
+        index = 0
+        for i in picked_word:
+            if i == player_guess:
+                correct[index] = player_guess
+            index += 1
+        replace_blank()
+        
+    else:
+        if player_guess not in incorrect:
+            incorrect.append(player_guess)
+        else:
+            print(f"You already guessed {player_guess}, try another letter.")
+    
