a4e0e045dfeb8ccecb5ce63f23181fa05c945d5a
GaryS007 <chattomeh@hotmail.com>
2022-12-23 19:28:50 +0000

    Add the ability to lose after 5 incorrect guesses and update inline comments. Pass code through beautifier

--- run.py
+++ run.py
@@ -5,11 +5,9 @@ words = ["orange", "apple"]  # List of words
 picked_word = random.choice(words)  # Picks a word from words
 length = len(picked_word)
 
-print(
-    f"Your random word has {length} letters"
-    )
+print(f"Your random word has {length} letters")
 
-correct = ['_']*len(picked_word)  # _ x length of word from picked_word
+correct = ["_"] * length  # _ x length of word from picked_word
 incorrect = []  # All incorrect guesses go here.
 
 
@@ -19,17 +17,15 @@ def replace_blank():
     in the "correct" variable with the player_guess in picked_word.
     """
     for i in correct:
-        print(i, end='')
+        print(i, end="")
     print()
 
 
 while True:
 
-    print('....................................')
+    print("....................................")
 
-    player_guess = input(
-        "Want to live? Then guess a letter: "
-    )
+    player_guess = input("Want to live? Then guess a letter: ")
 
     if player_guess in picked_word:
         # This if statement sets index to 0
@@ -38,20 +34,26 @@ while True:
         index = 0
         for i in picked_word:
             if i == player_guess:
-                correct[index] = player_guess   
+                correct[index] = player_guess
             index += 1
         replace_blank()
-        
+
     else:
         if player_guess not in incorrect:
-            # Checks if players guess is already guessed, 
+            # Checks if players guess is already guessed,
             # if not, append to incorrect list.
             incorrect.append(player_guess)
-            print(f"{player_guess} is not in the word")    
+            print(f"{player_guess} is not in the word")
+            print(f"Your incorrect guesses so far: {incorrect}")
         else:
             print(f"You already guessed {player_guess}, try another letter.")
-    if '_' not in correct:
+    if len(incorrect) == 5:
+        # incorrect is a list that contains the incorrect guesses of the player
+        # If the length of incorrect is equal to 5, game over.
+        print(f"RIP, better luck next time! The word was {picked_word}")
+        break
+    if "_" not in correct:
         # Checks if there is any underscores left in correct,
         # if no _ found, break from loop.
-        print('You win, there will be no hanging today!')
+        print("You win, there will be no hanging today!")
         break
