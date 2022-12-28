c103edb2923c646f00705567fa38552724a20b28
GaryS007 <chattomeh@hotmail.com>
2022-12-28 14:20:02 +0000

    Create hangman.py copy code into that file. Add isalpha(): to ensure only letters are accepted. Fix while statement

--- hangman.py
+++ hangman.py
@@ -29,7 +29,7 @@ while True:
     player_guess = input("Want to live? Then guess a letter: ")
     if player_guess.isalpha() is False:
         print("Only letters are allowed!")
-    if player_guess in picked_word:
+    elif player_guess in picked_word:
         # This if statement sets index to 0
         # and iterates through each letter within picked_word.
         # Then calls the function to replace _ with player_guess
@@ -40,18 +40,7 @@ while True:
             index += 1
         replace_blank()
         print(f"You guessed correct! {player_guess} is in the word")
-
-    else:
-        if player_guess not in incorrect:
-            # Checks if players guess is already guessed,
-            # if not, append to incorrect list.
-            incorrect.append(player_guess)
-            print(f"{player_guess} is not in the word, try again. \n")
-            print(f"Your incorrect guesses so far: {incorrect}")
-            replace_blank()
-        else:
-            print(f"You already guessed {player_guess}, try another letter.")
-    if len(incorrect) == 1:
+    elif len(incorrect) == 1:
         print(
             "   _____ \n"
             "  |     | \n"
@@ -62,7 +51,7 @@ while True:
             "  |      \n"
             "__|__\n"
         )
-    if len(incorrect) == 2:
+    elif len(incorrect) == 2:
         print(
             "   _____ \n"
             "  |     | \n"
@@ -73,7 +62,7 @@ while True:
             "  |      \n"
             "__|__\n"
         )
-    if len(incorrect) == 3:
+    elif len(incorrect) == 3:
         print(
             "   _____ \n"
             "  |     | \n"
@@ -84,7 +73,7 @@ while True:
             "  |      \n"
             "__|__\n"
         )
-    if len(incorrect) == 4:
+    elif len(incorrect) == 4:
         print(
             "   _____ \n"
             "  |     | \n"
@@ -95,7 +84,7 @@ while True:
             "  |      \n"
             "__|__\n"
         )
-    if len(incorrect) == 5:
+    elif len(incorrect) == 5:
         print(
             "   _____ \n"
             "  |     | \n"
@@ -108,8 +97,19 @@ while True:
         break
         # incorrect is a list that contains the incorrect guesses of the player
         # If the length of incorrect is equal to 5, game over.
-    if "_" not in correct:
+    elif "_" not in correct:
         # Checks if there is any underscores left in correct,
         # if no _ found, break from loop.
         print("You win, there will be no hanging today!")
         break
+    else:
+        if player_guess not in incorrect:
+            # Checks if players guess is already guessed,
+            # if not, append to incorrect list.
+            incorrect.append(player_guess)
+
+            print(f"{player_guess} is not in the word, try again. \n")
+            print(f"Your incorrect guesses so far: {incorrect}")
+            replace_blank()
+        else:
+            print(f"You already guessed {player_guess}, try another letter.")
